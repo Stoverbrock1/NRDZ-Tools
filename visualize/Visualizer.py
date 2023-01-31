daea788639aef618275a5f906e27f0d795fbea27
Stoverbrock1 <ztgstover@berkeley.edu>
2023-01-30 16:10:54 -0800

    Traté una otra vez añadir barra de color

--- visualize/Visualizer.py
+++ visualize/Visualizer.py
@@ -155,7 +155,7 @@ class dataManager:
 
 
                 plotInd += 1
-        plt.colorbar(label='PSD (dB/Hz)')
+        fig.colorbar(label='PSD (dB/Hz)')
         plt.tight_layout()
         plt.show()
 
