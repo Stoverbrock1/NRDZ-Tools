3fa02248a914c68e571f104e6996c62f6c5ce48a
Stoverbrock1 <ztgstover@berkeley.edu>
2023-02-02 14:00:22 -0800

    Edited color bar size location etc

--- visualize/Visualizer.py
+++ visualize/Visualizer.py
@@ -155,7 +155,7 @@ class dataManager:
 
 
                 plotInd += 1
-        caxs = fig.add_axes([.7, 0, .1, 1])
+        caxs = fig.add_axes([.9, 0, .08, .8])
         #plt.colorbar(im, cax = caxs)
         #plt.tight_layout()
         plt.show()
